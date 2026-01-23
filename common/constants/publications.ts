254156a37d87a057203a0f475c9916f22aeaf3b5
youssefmetwalli <ymetwalli22@gmail.com>
2026-01-23 17:27:43 +0900

    pdf url

--- common/constants/publications.ts
+++ common/constants/publications.ts
@@ -7,7 +7,7 @@ export const PUBLICATIONS: Publication[] = [
       authors: ["Youssef Obata", "Mohamed Hamada"],
       venue: "18th IEEE International Symposium on Embedded Systems, MCSoC, Singapore",
       date: "December, 2025",
-      url: "https://ieeexplore.ieee.org/document/11310863",
+      url: "https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=11310863",
       doi: "10.1109/MCSoC67473.2025.00080",
       code: "https://github.com/you/example-repo",
       tags: ["Intelligent Tutoring Systems", "Retrieval-Augmented Generation", "Automata Theory", "Educational Technology"]
