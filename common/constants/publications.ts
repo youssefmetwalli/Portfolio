d80a22c54cedb38aeb0e4403baed4187dc8ec779
youssefmetwalli <ymetwalli22@gmail.com>
2026-01-23 16:59:28 +0900

    add publication & doi

--- common/constants/publications.ts
+++ common/constants/publications.ts
@@ -7,8 +7,8 @@ export const PUBLICATIONS: Publication[] = [
       authors: ["Youssef Obata", "Mohamed Hamada"],
       venue: "18th IEEE International Symposium on Embedded Systems, MCSoC, Singapore",
       date: "December, 2025",
-      url: "https://example.com/paper.pdf",
-      doi: "10.9999/example-doi",
+      url: "https://ieeexplore.ieee.org/document/11310863",
+      doi: "10.1109/MCSoC67473.2025.00080",
       code: "https://github.com/you/example-repo",
       tags: ["Intelligent Tutoring Systems", "Retrieval-Augmented Generation", "Automata Theory", "Educational Technology"]
     },
