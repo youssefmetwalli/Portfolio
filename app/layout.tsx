4492c1d98373509d3019fced6325a1e3474d6439
youssefmetwalli <ymetwalli22@gmail.com>
2026-04-25 08:13:40 +0900

    update icon rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,23 @@ import NextAuthProvider from "@/SessionProvider";
 import { METADATA } from "@/common/constants/metadata";
 import { onestSans } from "@/common/styles/fonts";
 
+const siteUrl = process.env.DOMAIN?.startsWith("http")
+  ? process.env.DOMAIN
+  : "https://youssefobata.com";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(siteUrl),
   description: METADATA.description,
   keywords: METADATA.keyword,
   creator: METADATA.creator,
   authors: { name: METADATA.creator },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "512x512", type: "image/png" },
+      { url: "/icon.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
+  },
   openGraph: {
     images: METADATA.profile,
     siteName: METADATA.openGraph.siteName,
